de9693239600a7289b6fb103fd1365860afe7645
Bernie Chiu <bernie_chiu@hotmail.com>
2013-10-22 11:33:47 +0800

    Finish exercise 1

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,18 @@
 require 'erb'
 
 x = 42
+pets = { cat: "fierce", dog: "loyal" }
+furniture = ["windows", "curtain", "sofa"]
+
 template = ERB.new "The value of x is: <%= x %>"
+template_2 = ERB.new "Lots of pets here: <%= pets %>"
+template_3 = ERB.new("
+Lots of furniture:
+<% furniture.each do |item| %>
+  <%= item %>
+<% end %>
+")
+
 puts template.result(binding)
+puts template_2.result(binding)
+puts template_3.result(binding)
